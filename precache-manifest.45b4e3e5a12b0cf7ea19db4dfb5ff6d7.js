8376cc2931db4e273050b1949dba29fc17d557f3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-01 13:53:09 +0000

    Deploy sky172839465/test-app to github.com/sky172839465/test-app.git:gh-pages

---
+++ precache-manifest.45b4e3e5a12b0cf7ea19db4dfb5ff6d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "115cd2cf2d1dba5e5cd6",
+    "url": "/test-app/static/css/main.e8350a75.chunk.css"
+  },
+  {
+    "revision": "115cd2cf2d1dba5e5cd6",
+    "url": "/test-app/static/js/main.115cd2cf.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/test-app/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "3510dee076921078b779",
+    "url": "/test-app/static/js/runtime~main.3510dee0.js"
+  },
+  {
+    "revision": "536cb0b12cea8d64886768a539e668bf",
+    "url": "/test-app/index.html"
+  }
+];
